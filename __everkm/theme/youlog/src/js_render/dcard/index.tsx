e9f5db802e17ec86fe523546e04125ca9b1d6920
dayu <dayu@everkm.cn>
2025-10-29 22:54:06 +0800

    修复dcard 渲染

--- __everkm/theme/youlog/src/js_render/dcard/index.tsx
+++ __everkm/theme/youlog/src/js_render/dcard/index.tsx
@@ -1,12 +1,16 @@
+import { renderToStringAsync } from "solid-js/web";
 import DcardList from "./list";
 
-function renderDcard(name: string, props: any) {
-  switch (name) {
-    case "list2":
-      return <DcardList {...props} />;
-    default:
-      throw new Error(`Dcard ${name} not found`);
-  }
+async function renderDcard(name: string, props: any) {
+  let html = await renderToStringAsync(() => {
+    switch (name) {
+      case "list2":
+        return <DcardList {...props} />;
+      default:
+        throw new Error(`Dcard ${name} not found`);
+    }
+  });
+  return html;
 }
 
 export { renderDcard };
