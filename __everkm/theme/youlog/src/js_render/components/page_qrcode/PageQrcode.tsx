f3164e6629822df587919284d641890275c9e4ea
dayu <jcoldyu@gmail.com>
2025-11-13 16:40:07 +0800

    优化色彩

--- __everkm/theme/youlog/src/js_render/components/page_qrcode/PageQrcode.tsx
+++ __everkm/theme/youlog/src/js_render/components/page_qrcode/PageQrcode.tsx
@@ -7,7 +7,7 @@ import { Component } from "solid-js";
 const PageQrcode: Component = () => {
   return (
     <div class="md:flex flex-col items-center my-6 hidden print:flex">
-      <div data-el="page-qrcode" class="flex justify-center" />
+      <div data-el="page-qrcode" class="flex justify-center bg-white" />
       <p class="text-sm text-gray-600 dark:text-gray-400 text-center whitespace-nowrap w-full">
         扫码打开本页面
       </p>
